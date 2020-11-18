ccf726fd3126eb53d011ff74e8b9e203fd441ba1
Satish Singh <singhsatish7275@gmail.com>
2020-11-18 23:26:50 +0530

    components created

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <h2>
+      Track Expenses by your-munim
+    </h2>
+  )
+}
---
+++ src/components/TransactionList.js
@@ -0,0 +1,16 @@
+import React, { useContext} from 'react'
+import { GlobalContext } from './../context/GlobalState';
+
+export const TransactionList = () => {
+    const context = useContext(GlobalContext);
+    return (
+        <>
+            <h3>History</h3>
+            <ul className="list">
+                <li className="minus">
+                    Cash <span>-$400</span><button className="delete-btn">X</button>
+                </li>
+            </ul>
+        </>
+    )
+}
